9f0fb26deb701b36b911761d09e75cf604252434
0tae <choiyt3465@naver.com>
2025-09-25 23:21:20 +0900

    feat : EnableJpaAuditing 추가

--- src/main/java/com/example/hatsalvoids/HatsalvoidsApplication.java
+++ src/main/java/com/example/hatsalvoids/HatsalvoidsApplication.java
@@ -2,8 +2,10 @@ package com.example.hatsalvoids;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
+@EnableJpaAuditing
 public class HatsalvoidsApplication {
 
     public static void main(String[] args) {
